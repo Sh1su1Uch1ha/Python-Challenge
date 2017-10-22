ad8e4ade26e494f522d9a21178763e4b373950d9
Quinn McIntyre <quinnmcintyre@Ryans-iMac.lan>
2017-10-22 15:09:01 -0600

    First commit for Python Challenge, Woohoo!

---
+++ Levels/levelone.py
@@ -0,0 +1,3 @@
+message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+list_comprehension = [chr(ord(y)+2) for y in message]
+print "".join(list_comprehension)
